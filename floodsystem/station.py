15901c3bad383cf21a202476dc5f89a58ffcfd92
Weixuan Zhang <wz315@cam.ac.uk>
2020-01-18 23:25:17 +0000

    Using property decorator to prevent modifications

--- floodsystem/station.py
+++ floodsystem/station.py
@@ -1,4 +1,4 @@
-# Copyright (C) 2018 Garth N. Wells
+# Copyright (C) 2018 Garth N. Wells Modified by Weixuan Zhang
 #
 # SPDX-License-Identifier: MIT
 """This module provides a model for a monitoring station, and tools
@@ -13,21 +13,43 @@ class MonitoringStation:
     def __init__(self, station_id, measure_id, label, coord, typical_range,
                  river, town):
 
-        self.station_id = station_id
-        self.measure_id = measure_id
+        self._station_id = station_id
+        self._measure_id = measure_id
 
         # Handle case of erroneous data where data system returns
         # '[label, label]' rather than 'label'
-        self.name = label
+        self._name = label
         if isinstance(label, list):
-            self.name = label[0]
+            self._name = label[0]
 
-        self.coord = coord
-        self.typical_range = typical_range
-        self.river = river
-        self.town = town
+        self._coord = coord
+        self._typical_range = typical_range
+        self._river = river
+        self._town = town
 
         self.latest_level = None
+
+    @property
+    def station_id(self):
+        return self._station_id
+    @property
+    def measure_id(self):
+        return self._measure_id
+    @property
+    def name(self):
+        return self._name
+    @property
+    def coord(self):
+        return self._coord
+    @property
+    def typical_range(self):
+        return self._typical_range
+    @property
+    def river(self):
+        return self._river
+    @property
+    def town(self):
+        return self._town
 
     def __repr__(self):
         d = "Station name:     {}\n".format(self.name)

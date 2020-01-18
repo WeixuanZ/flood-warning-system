03a9a2651de82e55feebc5bdf59587e9834230c9
Weixuan Zhang <wz315@cam.ac.uk>
2020-01-18 21:30:42 +0000

    Updated description to Task1C demo

--- Task1C.py
+++ Task1C.py
@@ -8,7 +8,7 @@ from floodsystem.stationdata import build_station_list
 
 def run():
     '''
-    Prints a list of tuples (station name, town, distance) for the 10 closest and the 10 furthest stations from the Cambridge city centre, (52.2053, 0.1218).
+    Build a list of stations within 10 km of the Cambridge city centre. Print the names of the stations, listed in alphabetical order.
     '''
     stations = build_station_list()
     sorted_stations = stations_within_radius(stations, (52.2053, 0.1218), 10)

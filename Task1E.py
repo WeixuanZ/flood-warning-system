bbea8cf65bf336bf4885b01c0594ae39787b8d1d
Ghifari <ghifari@pradana.com>
2020-01-21 18:24:54 +0000

    Added tests for task F

--- Task1E.py
+++ Task1E.py
@@ -8,7 +8,7 @@ from floodsystem.stationdata import build_station_list
 
 def run():
     stations = build_station_list()
-    print(rivers_by_station_number(stations, 11))
+    print(rivers_by_station_number(stations, 9))
 
 
 if __name__ == "__main__":
--- test_geo.py
+++ test_geo.py
@@ -1,4 +1,4 @@
-# Copyright (C) 2020 Weixuan Zhang
+# Copyright (C) 2020 Weixuan Zhang & Ghifari Pradana
 #
 # SPDX-License-Identifier: MIT
 """Unit test for the geo module"""
@@ -94,27 +94,38 @@ class TestClass:
         stations_on_river = stations_by_river(stations)
         assert sorted(stations_on_river['test_river_2'], key=lambda x: x.name) == [station2, station3]
         assert stations_on_river['test_river_1'] == [station1]
-       
-	def rivers_by_station_number(self):
-	    station1 = MonitoringStation(station_id='test_station_id_1',
-                                     measure_id='test_measure_id_1',
-                                     label='Test Station 1',
-                                     coord=(0., 1.),
-                                     typical_range=(0., 1.),
-                                     river='test_river_1',
-                                     town='test_town_1')
-        station2 = MonitoringStation(station_id='test_station_id_2',
-                                     measure_id='test_measure_id_2',
-                                     label='Test Station 2',
-                                     coord=(1., 1.),
-                                     typical_range=(0., 1.),
-                                     river='test_river_2',
-                                     town='test_town_2')
-        station3 = MonitoringStation(station_id='test_station_id_3',
-                                     measure_id='test_measure_id_3',
-                                     label='Test Station 3',
-                                     coord=(10., 10.),
-                                     typical_range=(0., 1.),
-                                     river='test_river_2',
-                                     town='test_town_3')	
-		stations = [station1, station2, station3]
+    
+    def test_rivers_by_station_number(self):
+        station1 = MonitoringStation(station_id="111",
+                                     measure_id="111",
+                                     label="Station 1",
+                                     coord=(0,1),
+                                     typical_range=(0.1,0.4),
+                                     river="River 1",
+                                     town="Town 1")
+        station2 = MonitoringStation(station_id="222",
+                                     measure_id="222",
+                                     label="Station 2",
+                                     coord=(1,1),
+                                     typical_range=(0.3,0.8),
+                                     river="River 2",
+                                     town="Town 2")
+        station3 = MonitoringStation(station_id="333",
+                                     measure_id="333",
+                                     label="Station 3",
+                                     coord=(0,3),
+                                     typical_range=(0.43,0.88),
+                                     river="River 2",
+                                     town="Town 3")
+        station4 = MonitoringStation(station_id="444",
+                                     measure_id="444",
+                                     label="Station 4",
+                                     coord=(8,3),
+                                     typical_range=(0.1,0.9),
+                                     river="River 3",
+                                     town="Town 4")
+        assert rivers_by_station_number([station1], 1) == (("River 1",1))
+        assert rivers_by_station_number([station1, station2, station3],1) == (("River 2",2))
+        assert rivers_by_station_number([station1, station2, station3],2) == (("River 2",2),("River 1", 1))
+        assert rivers_by_station_number([station1, station2, station3,station4],3) == (("River 2",2),("River 1", 1), ("River 3",1))
+        assert rivers_by_station_number([station1, station2, station3,station4],4) == (("River 2",2),("River 1", 1), ("River 3",1))

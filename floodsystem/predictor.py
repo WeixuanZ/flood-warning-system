c4b9724c3a691d1f4c70c91ae34cd61eda568e65
Weixuan Zhang <wz315@cam.ac.uk>
2020-01-31 23:18:42 +0000

    Finished data fetching, model construction, training function, but input shape is currently not correct

--- floodsystem/predictor.py
+++ floodsystem/predictor.py
@@ -1,39 +1,84 @@
 import os
-os.environ["KERAS_BACKEND"] = "plaidml.keras.backend"
-import numpy as np
-from sklearn.preprocessing import MinMaxcaler
-import matplotlib.pyplot as plt
 
+os.environ["KERAS_BACKEND"] = "plaidml.keras.backend"
+
+import numpy as np
+import datetime
+from sklearn.preprocessing import MinMaxScaler
 import keras
 from keras.models import Sequential
 from keras.layers import Dense, LSTM, Dropout
-from keras.optimizers import SGD, Adam
 
-data = None
+from .datafetcher import fetch_measure_levels
+from .stationdata import build_station_list
 
-scaler = MinMaxcaler(feature_range=(0, 1))
-data_scaled = scaler.fit_transform(data)
+scalar = MinMaxScaler(feature_range=(0, 1))
+
+def fetch_levels(station_name, dt=100):
+    stations = build_station_list()
+    try:
+        station = next(s for s in stations if s.name == station_name)
+    except StopIteration:
+        print("Station {} could not be found".format(station_name))
+        return
+    _, data = fetch_measure_levels(station.measure_id, dt=datetime.timedelta(days=dt))
+    return np.array(data)
 
 
-def data_prep(data, num)
-    return np.array([data[i - num:i, 0] for i in range(num, len(data))]), np.array([data[i, 0] for i in range(num, len(data))])
+def data_prep(data, sample_size=60):
+    scaled_levels = scalar.fit_transform(data.reshape(-1, 1))
+    x = np.array([scaled_levels[i - sample_size:i, 0] for i in range(sample_size, len(scaled_levels))])
+    x = x.reshape(x.shape[0], 1, x.shape[1])  # samples, time steps, features
+    print(x.shape)
+    y = np.array([scaled_levels[i, 0] for i in range(sample_size, len(scaled_levels))])
+    return x, y
 
-x_train, y_train = data_prep(data, 10)
-x_train = np.reshape(x_train, (x_train.shape[0], x_train.shape[1], 1))  # samples, time steps, features
 
-model = Sequential()
-model.add(LSTM(units = 32, return_sequences = True, input_shape = (x_train.shape[1], 1), activation='tanh', recurrent_activation='sigmoid'))
-model.add(Dropout(0.2))
-model.add(LSTM(units = 64, activation='tanh', recurrent_activation='sigmoid'))
-model.add(Dropout(0.2))
-model.add(LSTM(units = 128, activation='tanh', recurrent_activation='sigmoid'))
-model.add(Dropout(0.4))
-model.add(Dense(1, activation='softmax'))
+def build_model(input_shape=60):
+    model = Sequential()
+    model.add(LSTM(units=32, return_sequences=True, input_shape=(input_shape,1), activation='tanh',
+                   recurrent_activation='sigmoid'))
+    model.add(Dropout(0.2))
+    model.add(LSTM(units=64, activation='tanh', recurrent_activation='sigmoid'))
+    model.add(Dropout(0.2))
+    model.add(LSTM(units=128, activation='tanh', recurrent_activation='sigmoid'))
+    model.add(Dropout(0.4))
+    model.add(Dense(1, activation='softmax'))
 
-model.compile(loss='mean_squared_error', optimizer='Adam', metrics=['accuracy'])
+    model.compile(loss='mean_squared_error', optimizer='Adam', metrics=['accuracy'])
 
-def train_model(model,batch_size=128, epoch=1000, save_file='../model/detector_model.hdf5'):
-	history = model.fit(x_train, y_train, batch_size=batch_size, epochs=epoch, verbose=1)
+    return model
 
-train_predict = model.predict()
-train_predict = scaler.inverse_transform(trainPredict)
+
+def train_model(model, x, y, batch_size=128, epoch=1000, save_file='./model/predictor_model.hdf5'):
+    model.fit(x, y, batch_size=batch_size, epochs=epoch, verbose=1)
+    model.save(save_file)
+    return model
+
+
+def predict(station_name, dataset_size=100, sample_size=60, iteration=10, use_pretrained=True):
+    if use_pretrained:
+        try:
+            model = keras.models.load_model('./model/predictor_model.hdf5')
+        except:
+            print('No pre-trained model found, training a model for {} now.'.format(station_name))
+            levels = fetch_levels(station_name, dataset_size)
+            x_train, y_train = data_prep(levels, sample_size)
+            model = train_model(build_model(), x_train, y_train)
+    else:
+        print('Training a model for {} now.'.format(station_name))
+        levels = fetch_levels(station_name, dataset_size)
+        x_train, y_train = data_prep(levels, sample_size)
+        model = train_model(build_model(), x_train, y_train)
+
+    predictions = []
+    levels = scalar.fit_transform(fetch_levels(station_name, sample_size).reshape(-1, 1))
+    for i in range(iteration):
+        prediction = model.predict(levels[-sample_size:])
+        predictions.append(prediction)
+        levels.append(prediction)
+
+    return fetch_levels(station_name, sample_size), scalar.inverse_transform(predictions)
+
+
+

241289843761548f3a7a2044568daf8df1bc246f
Weixuan Zhang <wz315@cam.ac.uk>
2020-02-01 13:58:38 +0000

    Modified model; Displaying demo prediction curve

---
+++ Predict.py
@@ -0,0 +1,15 @@
+import matplotlib.pyplot as plt
+import numpy as np
+
+from floodsystem.predictor import predict
+from floodsystem.stationdata import build_station_list
+
+stations = build_station_list()
+
+# data, demo, prediction = predict('Cam', dataset_size=1000, lookback=100, iteration=30, display=300, use_pretrained=False, batch_size=256, epoch=20)
+data, demo, prediction = predict(stations[25].name, dataset_size=1000, lookback=100, iteration=50, display=300, use_pretrained=False, batch_size=256, epoch=20)
+
+plt.plot(np.arange(1, len(data) + 1, 1), data)
+plt.plot(np.arange(1, len(data) + 1, 1), demo)
+plt.plot(np.arange(len(data) + 1, len(data) + len(prediction) + 1, 1), prediction)
+plt.show()
--- floodsystem/predictor.py
+++ floodsystem/predictor.py
@@ -17,7 +17,8 @@ scalar = MinMaxScaler(feature_range=(0, 1))
 np.random.seed(6)
 
 
-def fetch_levels(station_name, dt=100):
+# TODO: display time
+def fetch_levels(station_name, dt):
     stations = build_station_list()
     try:
         station = next(s for s in stations if s.name == station_name)
@@ -28,28 +29,26 @@ def fetch_levels(station_name, dt=100):
     return np.array(data)
 
 
-def data_prep(data, lookback=60):
-    scaled_levels = scalar.fit_transform(data.reshape(-1, 1))
+def data_prep(data, lookback):
+    scaled_levels = scalar.transform(data.reshape(-1, 1))
     x = np.array([scaled_levels[i - lookback:i, 0] for i in range(lookback, len(scaled_levels))])
     x = np.reshape(x, (x.shape[0], 1, x.shape[1]))  # samples, time steps, features
     y = np.array([scaled_levels[i, 0] for i in range(lookback, len(scaled_levels))])
     return x, y
 
 
-def build_model(lookback=60):
+def build_model(lookback):
     model = Sequential()
-    model.add(LSTM(units=32, return_sequences=True, input_shape=(1, lookback), activation='tanh'))
-    model.add(Dropout(0.2))
-    model.add(LSTM(units=64, activation='tanh'))
-    model.add(Dropout(0.2))
-    model.add(Dense(1, activation='sigmoid'))
-
-    model.compile(loss='mean_squared_error', optimizer='Adam')
+    model.add(LSTM(256, activation='relu', input_shape=(1, lookback), recurrent_dropout=0.1))
+    model.add(Dense(512, activation='relu'))
+    # model.add(Dense(256, activation='relu'))
+    model.add(Dense(1, activation='tanh'))
+    model.compile(optimizer='adam', loss='mse')
 
     return model
 
 
-def train_model(model, x, y, batch_size=128, epoch=1000, save_file='./floodsystem/cache/predictor_model.hdf5'):
+def train_model(model, x, y, batch_size, epoch, save_file='./floodsystem/cache/predictor_model.hdf5'):
     history = model.fit(x, y, batch_size=batch_size, epochs=epoch, verbose=1)
     plt.plot(history.history['loss'])
     plt.ylabel('loss')
@@ -62,28 +61,39 @@ def train_model(model, x, y, batch_size=128, epoch=1000, save_file='./floodsyste
     return model
 
 
-def predict(station_name, dataset_size=100, lookback=60, iteration=10, use_pretrained=True):
-
+def predict(station_name, dataset_size=500, lookback=20, iteration=30, display=100, use_pretrained=True, batch_size=128, epoch=100):
     levels = fetch_levels(station_name, dataset_size)
+    scalar.fit(levels.reshape(-1,1))  # fit the scalar on across the entire dataset
     if use_pretrained:
         try:
             model = keras.models.load_model('./floodsystem/cache/predictor_model.hdf5')
         except:
             print('No pre-trained model found, training a model for {} now.'.format(station_name))
             x_train, y_train = data_prep(levels, lookback)
-            model = train_model(build_model(lookback), x_train, y_train, epoch=100)
+            model = train_model(build_model(lookback), x_train, y_train, batch_size, epoch)
     else:
         print('Training a model for {} now.'.format(station_name))
         x_train, y_train = data_prep(levels, lookback)
-        model = train_model(build_model(lookback), x_train, y_train, epoch=100)
+        model = train_model(build_model(lookback), x_train, y_train, batch_size, epoch)
 
+    # prediction of future <iteration> readings, based on the last <lookback> values
     predictions = None
-    levels = scalar.fit_transform(levels[-lookback:].reshape(-1, 1))
+    levels = scalar.transform(levels[-lookback:].reshape(-1, 1))
     levels = levels.reshape(1, 1, lookback)
     for i in range(iteration):
         prediction = model.predict(levels)
         levels = np.append(levels[:, :, -lookback+1:], prediction.reshape(1, 1, 1), axis=2)
         predictions = np.append(predictions, prediction, axis=0) if predictions is not None else prediction
 
-    return fetch_levels(station_name, dataset_size)[-100:], scalar.inverse_transform(predictions)
+    # demo of prediction of the last 100 data points, which is based on the <lookback> values before the final 100 points
+    demo = None
+    levels = fetch_levels(station_name, dataset_size)
+    levels = scalar.transform(levels[-display-lookback:-display].reshape(-1, 1)).reshape(1, 1, lookback)
+    for i in range(display):
+        prediction = model.predict(levels)
+        levels = np.append(levels[:, :, -lookback+1:], prediction.reshape(1, 1, 1), axis=2)
+        demo = np.append(demo, prediction, axis=0) if demo is not None else prediction
+
+    # return on last 100 data points, the demo values, and future predictions
+    return fetch_levels(station_name, dataset_size)[-display:], scalar.inverse_transform(demo), scalar.inverse_transform(predictions)
 

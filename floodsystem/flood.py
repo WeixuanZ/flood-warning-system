75032155c07cd7f6d522bb2673f0cb61cd04407d
Ghifari <ghifari@pradana.com>
2020-02-01 00:46:15 +0000

    2C and 2D

---
+++ floodsystem/flood.py
@@ -0,0 +1,32 @@
+# Copyright (C) 2020 Weixuan Zhang & Ghifari Pradana
+#
+# SPDX-License-Identifier: MIT
+"""This module contains a collection of functions related to
+flooding.
+
+"""
+
+from .utils import sorted_by_key
+
+
+def stations_level_over_threshold(stations, tol):
+    """
+    Function that returns stations whose latest relative water level is over some threshold.
+    Args:
+        param1 (list): List of stations (type MonitoringStation).
+        param2 (float): The threshold relative water level.
+    Returns:
+        list: List of tuples in the format (station (type MonitoringStation), relative water level) sorted by the relative level in descending order.
+    """
+    available_data = [(station, station.relative_water_level()) for station in stations if
+                      station.relative_water_level() is not None]
+    print(available_data[3])
+    return sorted_by_key([i for i in available_data if i[1] > tol], 1, reverse=True)
+
+def stations_highest_rel_level(stations, N):
+    
+    
+    available_data = [(station, station.relative_water_level()) for station in stations if
+                      station.relative_water_level() is not None]
+    return [station[0] for station in [data for data in sorted_by_key(available_data,1,True)[:N]]]
+

ea5749b6fd6de6845bf339e52e768275f397cfbc
Weixuan Zhang <wz315@cam.ac.uk>
2020-02-11 22:25:25 +0000

    Transfered from matplotlib to bokeh; Flatten the predict function outputs

--- Predict.py
+++ Predict.py
@@ -1,15 +1,34 @@
-import matplotlib.pyplot as plt
 import numpy as np
+# import datetime
+# import pandas as pd
+
+from bokeh.io import output_file, show
+from bokeh.models import ColumnDataSource, DatetimeTickFormatter
+from bokeh.plotting import figure
+
 
 from floodsystem.predictor import predict
 from floodsystem.stationdata import build_station_list
 
 stations = build_station_list()
 
-# data, demo, prediction = predict('Cam', dataset_size=1000, lookback=2500, iteration=100, display=300, use_pretrained=False, batch_size=256, epoch=20)
-data, demo, prediction = predict(stations[5].name, dataset_size=1000, lookback=2000, iteration=100, display=300, use_pretrained=False, batch_size=256, epoch=20)
+# date, data, demo, prediction = predict('Cam', dataset_size=1000, lookback=2000, iteration=100, display=300, use_pretrained=True, batch_size=256, epoch=20)
+date, data = predict(stations[5].name, dataset_size=1000, lookback=2000, iteration=100, display=300, use_pretrained=True, batch_size=256, epoch=20)
 
-plt.plot(np.arange(1, len(data) + 1, 1), data)
-plt.plot(np.arange(1, len(data) + 1, 1), demo)
-plt.plot(np.arange(len(data) + 1, len(data) + len(prediction) + 1, 1), prediction)
-plt.show()
+output_file("prediction.html")
+p = figure(plot_width = 600, plot_height = 600,
+           title='Prediction',
+           x_axis_label='Date', y_axis_label='Water level (m)')
+
+p.line(date[0], data[0], legend_label='Raw', line_width=2)
+p.line(date[0], data[1], line_color="orange", line_width=2, legend_label='Demo')
+p.line(date[1], data[2], line_color="green", line_width=2, legend_label='Prediction')
+p.xaxis.formatter = DatetimeTickFormatter(
+    hours=["%d %B %Y"],
+    days=["%d %B %Y"],
+    months=["%d %B %Y"],
+    years=["%d %B %Y"],
+    )
+p.xaxis.major_label_orientation = np.pi/4
+show(p)
+

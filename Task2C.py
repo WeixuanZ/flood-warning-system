f84ad542e21de8f2ae9dc59ec632278f960598a2
Weixuan Zhang <wz315@cam.ac.uk>
2020-02-11 21:21:21 +0000

    Reformatted code

--- Task2C.py
+++ Task2C.py
@@ -5,13 +5,14 @@
 from floodsystem.flood import stations_highest_rel_level
 from floodsystem.stationdata import build_station_list, update_water_levels
 
-def run():
 
+def run():
     stations = build_station_list()
     update_water_levels(stations)
-    high_risk_stations = stations_highest_rel_level(stations,10)
+    high_risk_stations = stations_highest_rel_level(stations, 10)
     for station in high_risk_stations:
         print(station.name, station.relative_water_level())
+
 
 if __name__ == "__main__":
     print("*** Task 2C: CUED Part IA Flood Warning System ***")
--- floodsystem/flood.py
+++ floodsystem/flood.py
@@ -22,6 +22,7 @@ def stations_level_over_threshold(stations, tol):
                       station.relative_water_level() is not None]
     return sorted_by_key([i for i in available_data if i[1] > tol], 1, reverse=True)
 
+
 def stations_highest_rel_level(stations, N):
     """
     Function that returns the N number of most at risk stations.
@@ -31,8 +32,7 @@ def stations_highest_rel_level(stations, N):
     Returns:
         list: List of stations(type MonitoringStation)
     """
-    
+
     available_data = [(station, station.relative_water_level()) for station in stations if
                       station.relative_water_level() is not None]
-    return [station[0] for station in [data for data in sorted_by_key(available_data,1,True)[:N]]]
-
+    return [station[0] for station in [data for data in sorted_by_key(available_data, 1, True)[:N]]]

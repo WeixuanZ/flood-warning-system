ff40c2ec778a2b7871e539917d4b46dfaf9fe99c
Weixuan Zhang <wz315@cam.ac.uk>
2020-02-11 21:50:28 +0000

    Plotting functions return the plot instead of showing it, preparing for combination; Modified other files and test accordingly

--- Map.py
+++ Map.py
@@ -2,15 +2,16 @@
 #
 # SPDX-License-Identifier: MIT
 
+from bokeh.io import show
+
 from floodsystem.plot import Map
 from floodsystem.stationdata import build_station_list
 
 
 def run():
     stations = build_station_list()
-    location_map = Map(stations)
-    location_map.build()
-
+    location_map = Map(stations).build()
+    show(location_map)
 
 if __name__ == "__main__":
     print("*** Extension: CUED Part IA Flood Warning System ***")

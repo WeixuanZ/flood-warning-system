4acd4dd41f5738635a8687bee2e9cd88bb4620e0
Weixuan Zhang <wz315@cam.ac.uk>
2020-02-13 00:32:32 +0000

    Showing predictions as tabs

--- main.py
+++ main.py
@@ -2,7 +2,7 @@ import numpy as np
 
 from bokeh.io import output_file, show
 from bokeh.layouts import gridplot, layout, column
-from bokeh.models import ColumnDataSource, Select, Div, TextInput, Button
+from bokeh.models import ColumnDataSource, Select, Div, TextInput, Tabs, Panel
 from bokeh.plotting import curdoc
 
 from floodsystem.plot import *
@@ -71,19 +71,22 @@ selected_plot.sizing_mode = 'scale_width'
 
 # Prediction
 
-predict_text = Div(text="""<p>Choose one of the high risk stations for prediction using a recurrent neural network.</p>""")
-predict_select = Select(title="Station to predict:", value=highrisk_stations[0].name, options=[i.name for i in highrisk_stations])
+predict_text = Div(text="""<p>Choose one of the high risk stations to see the prediction by a recurrent neural network.</p>""")
 
+predict_date = []
+predict_level = []
+predict_plots = []
+for station in highrisk_stations:
+    date, level = predict(station.name, dataset_size=1000, lookback=2000, iteration=100, display=300,
+                         use_pretrained=False, batch_size=256, epoch=20)
+    predict_plot = plot_prediction(date, level)
+    predict_plot.plot_width = 400
+    predict_plot.plot_height = 400
+    predict_plot.sizing_mode = 'scale_width'
+    predict_plots.append(Panel(child=predict_plot, title=station.name))
 
-class Source:
-    date, data = predict(highrisk_stations[0].name, dataset_size=1000, lookback=2000, iteration=100, display=300,
-                         use_pretrained=True, batch_size=256, epoch=20)
+predict_tabs = Tabs(tabs=predict_plots)
 
-
-predict_plot = plot_prediction(Source.date, Source.data)
-predict_plot.plot_width = 400
-predict_plot.plot_height = 400
-predict_plot.sizing_mode = 'scale_width'
 
 
 # Layout
@@ -95,7 +98,7 @@ select_column.sizing_mode = "fixed"
 highrisk_column = column(highrisk_title, highrisk_plots, width=800, height=600)
 highrisk_column.sizing_mode = "fixed"
 
-predict_column = column(predict_text, predict_select, predict_plot, width=500, height=600)
+predict_column = column(predict_text, predict_tabs, width=500, height=600)
 predict_column.sizing_mode = "fixed"
 
 l = layout([

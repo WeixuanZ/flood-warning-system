0fc0e6d8c50c17ec531ceb82c1e1e8c61e581795
Ghifari <ghifari@pradana.com>
2020-02-17 23:40:55 +0000

    Added docstring ffor each function

--- floodsystem/plot.py
+++ floodsystem/plot.py
@@ -135,7 +135,16 @@ def plot_prediction(date, data):
     return p
 
 def plot_water_level_with_fit(station, dates, levels, p):
-
+    """
+    Function that makes a graph of the water level over time for a given station with a least-square fit polynomial with a degree of p.
+    Args: 
+        param1 (MonitoringStation): The desired station to graph.
+        param2 (list): The list of dates for the x-axis.
+        param3 (list): The corresponding water level for each date, y-axis.
+        param4 (int): The degree of polynomial that is desired.
+    Returns:
+        Bokeh plot object.
+    """
     output_file(station.name + ".html")
     graph = figure(title=station.name, x_axis_label="Date", y_axis_label="Water level (m)")
     graph.line(dates, levels, line_width=2)

ebdd0b0ba95c48d8d8268aaefbffc5379fbc56c8
Ghifari <ghifari@pradana.com>
2020-02-18 21:18:29 +0000

    Added testing

--- test_plot.py
+++ test_plot.py
@@ -6,7 +6,7 @@ from os import path
 
 from floodsystem.plot import *
 from floodsystem.station import MonitoringStation
-
+from matplotlib.dates import num2date
 
 class TestClass:
 
@@ -32,3 +32,17 @@ class TestClass:
                                      town='test_town_1')
         show(plot_water_levels(station1, [0, 1, 2], [0.1, 0.2, 0.3]))
         assert path.isfile(station1.name + ".html")
+
+    def plot_water_level_with_fit(self):
+        station1 = MonitoringStation(station_id='test_station_id_1',
+                                     measure_id='test_measure_id_1',
+                                     label='Test Station 1',
+                                     coord=(0., 1.),
+                                     typical_range=(0., 1.),
+                                     river='test_river_1',
+                                     town='test_town_1')
+        dates = num2date([5,4,3,2,1])
+        levels = [16,9,4,1,0]
+
+        _ = plot_water_level_with_fit(station1,dates,levels,2)
+        assert path.isfile(station1.name + ".html") == True

675524234fa84b32d348b1c29b923196b6dc93a1
Weixuan Zhang <wz315@cam.ac.uk>
2020-02-19 18:30:53 +0000

    Added polyfit to app

--- floodsystem/analysis.py
+++ floodsystem/analysis.py
@@ -20,7 +20,7 @@ def polyfit(dates, levels, p):
     dates_num = date2num(dates)
     x = [date - dates_num[-1] for date in dates_num]
     y = levels
-    print([x, y, len(x), len(y)])
+    # print([x, y, len(x), len(y)])
     p_coeff = np.polyfit(x, y, p)
     poly = np.poly1d(p_coeff)
     return poly, dates_num[-1]

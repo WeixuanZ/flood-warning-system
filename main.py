cf5e34fa69f0b55443e265aa8e338b84b2ad4a44
Weixuan Zhang <wz315@cam.ac.uk>
2020-02-23 02:32:13 +0000

    Reformatted code

--- main.py
+++ main.py
@@ -4,26 +4,25 @@
 
 from datetime import timedelta
 from os import environ
-import numpy as np
 
+import numpy as np
 from bokeh.layouts import layout, column, row
-from bokeh.models import ColumnDataSource, Div, TextInput, Tabs, Panel, TapTool, HoverTool, GMapOptions, ColorBar, DataTable, TableColumn
+from bokeh.models import ColumnDataSource, Div, TextInput, Tabs, Panel, TapTool, HoverTool, GMapOptions, ColorBar, \
+    DataTable, TableColumn
+from bokeh.palettes import Spectral10, Turbo256, linear_palette
 from bokeh.plotting import curdoc, gmap
 from bokeh.transform import log_cmap
-from bokeh.palettes import Spectral10, Turbo256, linear_palette
 from fuzzywuzzy import process
 from matplotlib.dates import date2num
 from sklearn.cluster import DBSCAN
 
+from floodsystem.analysis import polyfit
 from floodsystem.datafetcher import fetch_measure_levels
 from floodsystem.flood import stations_highest_rel_level, stations_level_over_threshold
+from floodsystem.geo import rivers_by_station_number, rivers_with_station
 from floodsystem.plot import map_palette, plot_water_levels_dynamic, plot_water_levels_multiple, plot_prediction
 from floodsystem.predictor import predict
 from floodsystem.stationdata import build_station_list, update_water_levels
-from floodsystem.analysis import polyfit
-from floodsystem.geo import rivers_by_station_number, rivers_with_station
-
-# map_select = True
 
 ## Fetching and preparing data
 
@@ -34,21 +33,23 @@ highrisk_stations = stations_highest_rel_level(stations, 6)
 
 def convert_to_datasource(stations):
     return ColumnDataSource(data=dict(lat=[i.coord[0] for i in stations],
-                                    lng=[i.coord[1] for i in stations],
-                                    name=[i.name for i in stations],
-                                    measure_id=[i.measure_id for i in stations],
-                                    river=[i.river for i in stations],
-                                    town=[i.town for i in stations],
-                                    typical_low=[i.typical_range[0] if i.typical_range is not None else 'nan' for i in
-                                                 stations],
-                                    typical_high=[i.typical_range[1] if i.typical_range is not None else 'nan' for i in
-                                                  stations],
-                                    latest_level=[i.latest_level if i.latest_level is not None else 'nan' for i in
-                                                  stations],
-                                    relative_level=[
-                                        i.relative_water_level() if i.relative_water_level() is not None else 'nan' for
-                                        i in stations],
-                                    color=[map_palette(i) for i in stations]))
+                                      lng=[i.coord[1] for i in stations],
+                                      name=[i.name for i in stations],
+                                      measure_id=[i.measure_id for i in stations],
+                                      river=[i.river for i in stations],
+                                      town=[i.town for i in stations],
+                                      typical_low=[i.typical_range[0] if i.typical_range is not None else 'nan' for i in
+                                                   stations],
+                                      typical_high=[i.typical_range[1] if i.typical_range is not None else 'nan' for i
+                                                    in
+                                                    stations],
+                                      latest_level=[i.latest_level if i.latest_level is not None else 'nan' for i in
+                                                    stations],
+                                      relative_level=[
+                                          i.relative_water_level() if i.relative_water_level() is not None else 'nan'
+                                          for
+                                          i in stations],
+                                      color=[map_palette(i) for i in stations]))
 
 
 source = convert_to_datasource(stations)
@@ -173,7 +174,8 @@ for station in highrisk_stations:
         date, level = [], []
     predict_plot = plot_prediction(date, level)
     poly, d0 = polyfit(date[0], level[0], 4)
-    predict_plot.line(date[0]+date[1], [poly(date - d0) for date in date2num(date[0]+date[1])], line_width=2, line_color='gray', legend_label='Polynomial Fit', line_dash='dashed')
+    predict_plot.line(date[0] + date[1], [poly(date - d0) for date in date2num(date[0] + date[1])], line_width=2,
+                      line_color='gray', legend_label='Polynomial Fit', line_dash='dashed')
     predict_plot.plot_width = 400
     predict_plot.plot_height = 400
     predict_plot.sizing_mode = 'scale_width'
@@ -181,19 +183,21 @@ for station in highrisk_stations:
 
 predict_tabs = Tabs(tabs=predict_plots)
 
-
 ## Warning
 
-warning_text = Div(text="""<h3>Flooding Warnings</h3><p>All the stations with relative water level above 1.2 are shown in the map below. DBSCAN clustering algorithm is used.</p> """)
+warning_text = Div(
+    text="""<h3>Flooding Warnings</h3><p>All the stations with relative water level above 1.2 are shown in the map below. DBSCAN clustering algorithm is used.</p> """)
 
 risky_stations_with_level = stations_level_over_threshold(stations, 1.2)
 risky_stations = [i[0] for i in risky_stations_with_level]
 risky_source = convert_to_datasource(risky_stations)
 
-mapper = log_cmap(field_name='relative_level', palette=Spectral10, low=1.0, high=risky_stations[0].relative_water_level())
+mapper = log_cmap(field_name='relative_level', palette=Spectral10, low=1.0,
+                  high=risky_stations[0].relative_water_level())
 origin2 = (52.561928, -1.464854)
 options2 = GMapOptions(lat=origin2[0], lng=origin2[1], map_type="roadmap", zoom=5)
-location_map2 = gmap(environ.get('API_KEY'), options2, title="Stations above typical range", tools=tools, active_scroll="wheel_zoom")
+location_map2 = gmap(environ.get('API_KEY'), options2, title="Stations above typical range", tools=tools,
+                     active_scroll="wheel_zoom")
 r2 = location_map2.circle(x="lng", y="lat", size=15, color=mapper, fill_alpha=0.5, source=risky_source)
 hover_tool = HoverTool(tooltips=[
     ("Station Name", "@name"),
@@ -204,12 +208,11 @@ hover_tool = HoverTool(tooltips=[
     ("Latest Level (m)", "@latest_level")
 ])
 location_map2.add_tools(hover_tool)
-color_bar = ColorBar(color_mapper=mapper['transform'], width=8,  location=(0,0))
+color_bar = ColorBar(color_mapper=mapper['transform'], width=8, location=(0, 0))
 location_map2.add_layout(color_bar, 'right')
 location_map2.plot_width = 700
 location_map2.plot_height = 500
 location_map2.sizing_mode = 'scale_width'
-
 
 # Clustering
 
@@ -223,7 +226,7 @@ db = DBSCAN(eps=0.1, min_samples=5, metric='haversine', n_jobs=-1).fit(X)
 labels = db.labels_
 unique_labels = set(labels)
 num_clusters = len(unique_labels) - (1 if -1 in labels else 0)
-print('Number of clusters:'+str(num_clusters))
+print('Number of clusters:' + str(num_clusters))
 
 cluster_pallet = linear_palette(Turbo256, len(unique_labels))
 label_to_stations = dict()  # to find the list of stations knowing the cluster label
@@ -239,22 +242,24 @@ for i in unique_labels:
         coord = X[class_member_mask]
         for xy in coord:
             location_map3.circle(x=xy[1], y=xy[0], size=15, color=cluster_pallet[i], fill_alpha=0.8)
-            label_to_stations[i].append(coord_to_station[(xy[0], xy[1])])  # find the station from its coordinates and append it to the dictionary
+            label_to_stations[i].append(coord_to_station[(
+                xy[0], xy[1])])  # find the station from its coordinates and append it to the dictionary
 
 location_map3.plot_width = 700
 location_map3.plot_height = 500
 location_map3.sizing_mode = 'scale_width'
 
 # Risky rivers
-warning_text2 = Div(text="""<p><b>{}</b> rivers with risky stations, the top 5 is tabulated below.</p>""".format(len(rivers_with_station(risky_stations))), width=600)
+warning_text2 = Div(text="""<p><b>{}</b> rivers with risky stations, the top 5 is tabulated below.</p>""".format(
+    len(rivers_with_station(risky_stations))), width=600)
 warning_text2.sizing_mode = 'scale_width'
 
 risky_rivers = rivers_by_station_number(risky_stations, 5)
 risky_rivers_source = ColumnDataSource(data=dict(name=[i[0] for i in risky_rivers], num=[i[1] for i in risky_rivers]))
 risky_river_table_columns = [
-        TableColumn(field="name", title="River Name"),
-        TableColumn(field="num", title="Number of Risky Stations"),
-    ]
+    TableColumn(field="name", title="River Name"),
+    TableColumn(field="num", title="Number of Risky Stations"),
+]
 risky_river_table = DataTable(source=risky_rivers_source, columns=risky_river_table_columns, width=500, height=200)
 risky_river_table.sizing_mode = 'scale_width'
 
@@ -271,47 +276,32 @@ for label, s in label_to_stations.items():
 
 risky_towns = set(risky_towns)  # to find the total number of risky towns
 # sort the towns by the mean relative water level of the cluster it is in
-key_station_in_cluster = sorted(key_station_in_cluster, key=lambda x: mean_levels[key_station_in_cluster.index(x)], reverse=True)
+key_station_in_cluster = sorted(key_station_in_cluster, key=lambda x: mean_levels[key_station_in_cluster.index(x)],
+                                reverse=True)
 mean_levels = sorted(mean_levels, reverse=True)
 
-risky_towns_source = ColumnDataSource(data=dict(key_stations = [i.name for i in key_station_in_cluster], key_towns=[i.town for i in key_station_in_cluster], levels=[round(i.relative_water_level(),2) for i in key_station_in_cluster], mean=[round(i,2) for i in mean_levels]))
-warning_text3 = Div(text="""<p><b>{}</b> clusters found, the towns within these clusters (<b>{}</b> in total) have a risk of flooding. The table below lists the towns with the highest relative water level within each cluster in the order of decreasing risk (by calculating the mean relative water level of each cluster).</p>""".format(num_clusters, len(risky_towns)), width=600)
+risky_towns_source = ColumnDataSource(
+    data=dict(key_stations=[i.name for i in key_station_in_cluster], key_towns=[i.town for i in key_station_in_cluster],
+              levels=[round(i.relative_water_level(), 2) for i in key_station_in_cluster],
+              mean=[round(i, 2) for i in mean_levels]))
+warning_text3 = Div(
+    text="""<p><b>{}</b> clusters found, the towns within these clusters (<b>{}</b> in total) have a risk of flooding. The table below lists the towns with the highest relative water level within each cluster in the order of decreasing risk (by calculating the mean relative water level of each cluster).</p>""".format(
+        num_clusters, len(risky_towns)), width=600)
 risky_town_table_columns = [
-        TableColumn(field="key_towns", title="Towns with Highest Risk"),
-        TableColumn(field="key_stations", title="Station Name"),
-        TableColumn(field="levels", title="Relative Water Level"),
-        TableColumn(field="mean", title="Mean Cluster Relative Water Level"),
-    ]
+    TableColumn(field="key_towns", title="Towns with Highest Risk"),
+    TableColumn(field="key_stations", title="Station Name"),
+    TableColumn(field="levels", title="Relative Water Level"),
+    TableColumn(field="mean", title="Mean Cluster Relative Water Level"),
+]
 risky_town_table = DataTable(source=risky_towns_source, columns=risky_town_table_columns, width=500, height=200)
 risky_town_table.sizing_mode = 'scale_width'
-
-
 
 ## Layout
 
 map_column = column(location_map, width=700, height=500)
 
-# radio_button_group = RadioButtonGroup(labels=["Click on Map", "Search"], active=0)
-# select_column = column(select_text, radio_button_group, selected_plot, width=600, height=500)
 select_column = column(select_text, select_input, selected_plot, width=600, height=500)
 select_column.sizing_mode = "fixed"
-
-# def update_toggle():
-#     global map_select
-#     if radio_button_group.active == 1:
-#         map_select = False
-#         location_map.tools.pop()
-#         r.data_source.selected.indices = []
-#         select_column.children = [select_text, radio_button_group, select_input, selected_plot]
-#     else:
-#         map_select = True
-#         location_map.add_tools(tap_tool)
-#         r.data_source.selected.indices = []
-#         select_column.children = [select_text, radio_button_group, selected_plot]
-#
-#
-# radio_button_group.on_change('active', lambda attr, old, new: update_toggle())
-
 
 highrisk_column = column(highrisk_title, highrisk_plots, width=800, height=650)
 highrisk_column.sizing_mode = "fixed"
@@ -327,7 +317,9 @@ predict_column.sizing_mode = "fixed"
 town_column = column(warning_text3, width=650, height=70)
 predict_column.sizing_mode = "fixed"
 
-notice = Div(text="""<footer>&copy; Copyright 2020 Weixuan Zhang, Ghifari Pradana. CUED Part 1A Lent computing project.</footer>""", width=600)
+notice = Div(
+    text="""<footer>&copy; Copyright 2020 Weixuan Zhang, Ghifari Pradana. CUED Part 1A Lent computing project.</footer>""",
+    width=600)
 
 l = layout([
     [location_map, select_column],

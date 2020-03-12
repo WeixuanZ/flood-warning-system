5ba95e528c49cf71141de515dbe9638ddd814019
Weixuan Zhang <wz315@cam.ac.uk>
2020-03-12 19:12:08 +0000

    Updated doctrings of provided code to Google style

--- floodsystem/analysis.py
+++ floodsystem/analysis.py
@@ -1,6 +1,8 @@
 # Copyright (C) 2020 Ghifari Pradana
 #
 # SPDX-License-Identifier: MIT
+"""This module contains function for polynomial fitting on data
+"""
 import numpy as np
 from matplotlib.dates import date2num
 
@@ -21,7 +23,6 @@ def polyfit(dates, levels, p):
     dates_num = date2num(dates)
     x = [date - dates_num[-1] for date in dates_num]
     y = levels
-    # print([x, y, len(x), len(y)])
     p_coeff = np.polyfit(x, y, p)
     poly = np.poly1d(p_coeff)
     return poly, dates_num[-1]
--- floodsystem/utils.py
+++ floodsystem/utils.py
@@ -8,18 +8,27 @@
 
 def sorted_by_key(x, i, reverse=False):
     """For a list of lists/tuples, return list sorted by the ith
-    component of the list/tuple, E.g.
+    component of the list/tuple, 
+    
+    Examples:
+        Sort on first entry of tuple:
 
-    Sort on first entry of tuple:
+            >>> sorted_by_key([(1, 2), (5, 1)], 0)
+            [(1, 2), (5, 1)]
 
-    >>> sorted_by_key([(1, 2), (5, 1)], 0)
-    [(1, 2), (5, 1)]
-
-    Sort on second entry of tuple:
-
-    >>> sorted_by_key([(1, 2), (5, 1)], 1)
-    [(5, 1), (1, 2)]
-
+        Sort on second entry of tuple:
+        
+            >>> sorted_by_key([(1, 2), (5, 1)], 1)
+            [(5, 1), (1, 2)]
+    
+    Args:
+        x (list): A list of lists/tuples to be sorted
+        i (int): Sort according to which element of the inner lists/tuples
+        reverse (bool, optional): Default False
+    
+    Returns:
+        list: Sorted list of lists/tuples
+    
     """
 
     return sorted(x, key=lambda x: x[i], reverse=reverse)

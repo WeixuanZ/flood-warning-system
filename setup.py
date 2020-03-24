ee13e4dafda5a4c2068ebf41eb53c9a5c99f0c1d
Weixuan Zhang <wz315@cam.ac.uk>
2020-03-24 11:33:50 +0000

    Proper packaging, enabling tasks and tests to be moved into subdirectories; Updated .gitignore, .gitlab-ci.yml

--- setup.py
+++ setup.py
@@ -1,6 +1,14 @@
 from setuptools import setup
+
+with open("README.md", "r") as f:
+    long_description = f.read()
+
 setup(name='floodsystem',
-      version='0.1',
+      version='1.0',
+      author="Weixuan Zhang, Ghifari Pradana",
       description='CUED Part IA flood warning system exercise',
+      long_description=long_description,
+      url="https://github.com/WeixuanZ/flood-warning-system",
       packages=['floodsystem'],
+      classifiers=["License :: OSI Approved :: MIT License"]
       )
